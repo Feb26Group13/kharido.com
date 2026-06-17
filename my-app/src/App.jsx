2fb3bf61284940e23c39a096ceb20135de037501
Harsh Amarsingh Rajput <harsh.r.6541@gmail.com>
2026-06-17 18:53:01 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,61 +1,48 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomeComp from './components/HomeComp.jsx'
-import ProtectedRoutes from './components/ProtectedRoutes.jsx'
-import UserDashBoard from './components/UserDashBoard.jsx'
-import AdminDashBoard from './components/AdminDashBoard.jsx'
-import LogoutComp from './components/Logout.jsx'
-import SellerDashboard from './components/SellerDashboard.jsx'
-import SellerProducts from './components/SellerProducts.jsx'
-import AddProduct from './components/AddProduct.jsx'
-import SellerOrders from './components/SellerOrders.jsx'
-import SellerProfile from './components/SellerProfile.jsx'
-import SellerRegisterComp from './components/SellerRegisterComp.jsx'
-import RegisterChoice from './components/RegisterChoice.jsx'
 
-import HomeComp from './components/HomeComp'
-import LoginComp from './components/LoginComp'
-
-import CustomerRegisterComp from './components/CustomerRegisterComp'
-import SellerRegisterComp from './components/SellerRegisterComp'
-
+// Layout & Route Guards
 import ProtectedRoutes from './components/ProtectedRoutes'
 
-import ProductList from './components/ProductList';
-import ProductDetails from './components/ProductDetails';
+// Public Components
+import HomeComp from './components/HomeComp'
+import LoginComp from './components/LoginComp'
+import RegisterChoice from './components/RegisterChoice'
+import CustomerRegisterComp from './components/CustomerRegisterComp'
+import SellerRegisterComp from './components/SellerRegisterComp'
+import LogoutComp from './components/Logout'
 
+// Product Components
+import ProductList from './components/ProductList'
+import ProductDetails from './components/ProductDetails'
+
+// Dashboard Components
 import UserDashboard from './components/UserDashboard'
 import AdminDashboard from './components/AdminDashboard'
 import SellerDashboard from './components/SellerDashboard'
 
-import LogoutComp from './components/Logout'
-//this is a root component
+// Seller Sub-Components
+import SellerProducts from './components/SellerProducts'
+import AddProduct from './components/AddProduct'
+import SellerOrders from './components/SellerOrders'
+import SellerProfile from './components/SellerProfile'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/products" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetails/>}/>
-
-        {/* Public Routes */}
-
+        {/* --- Public Routes --- */}
         <Route path="/" element={<HomeComp />} />
-
         <Route path="/login" element={<LoginComp />} />
+        <Route path="/register" element={<RegisterChoice />} />
+        <Route path="/register/customer" element={<CustomerRegisterComp />} />
+        <Route path="/register/seller" element={<SellerRegisterComp />} />
 
-        <Route
-          path="/register/customer"
-          element={<CustomerRegisterComp />}
-        />
+        {/* --- Product Routes --- */}
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
 
-        <Route
-          path="/register/seller"
-          element={<SellerRegisterComp />}
-        />
-
-        {/* Customer Routes */}
-
+        {/* --- Customer Routes (Role 3) --- */}
         <Route
           path="/user"
           element={
@@ -70,28 +57,7 @@ function App() {
           <Route path="logout" element={<LogoutComp />} />
         </Route>
 
-        {/* Seller Routes */}
-    <>
-     <BrowserRouter>
-        <Routes>
-          {/* Registration Routes */}
-          {/* <Route path="register" element={<h1>Select Registration Type</h1>} /> */}
-          {/* <Route path="register/user" element={<UserRegisterComp />} /> */}
-          <Route path="register/seller" element={<SellerRegisterComp />} />
-                  <Route path="/" element={<HomeComp/>}/>
-                 <Route path="login" element={<LoginComp/>}/>
-                 <Route path="register" element={<RegisterChoice />} />
-                  <Route path="register/seller" element={<SellerRegisterComp />} />
-                 {/* <Route path="register" element={<h1> Registrtion Form </h1>}/> */}
-        
-        <Route path="/user" element={<ProtectedRoutes role={2}><UserDashBoard/></ProtectedRoutes>}>
-        <Route path="users" element={<h1> Users</h1>} />
-                <Route path="reports" element={ <h1> Reports</h1>} />
-                <Route path="logout" element={<LogoutComp/>} /> 
-        </Route>
-
-        {/* Admin Routes */}
-
+        {/* --- Admin Routes (Role 1) --- */}
         <Route
           path="/admin"
           element={
@@ -107,28 +73,25 @@ function App() {
           <Route path="refunds" element={<h1>Refunds</h1>} />
           <Route path="logout" element={<LogoutComp />} />
         </Route>
-        {/* Seller Routes */}
-            <Route
-              path="/seller"
-              element={
-                <ProtectedRoutes role={2}>
-                  <SellerDashboard />
-                </ProtectedRoutes>
-              }
-            >
-              <Route path="products" element={<SellerProducts />} />
-              <Route path="add-product" element={<AddProduct />} />
-              <Route path="orders" element={<SellerOrders />} />
-              <Route path="profile" element={<SellerProfile />} />
-              <Route path="logout" element={<LogoutComp />} />
-            </Route>
-        </Routes>
-        
 
-        {/* Invalid Route */}
+        {/* --- Seller Routes (Role 2) --- */}
+        <Route
+          path="/seller"
+          element={
+            <ProtectedRoutes role={2}>
+              <SellerDashboard />
+            </ProtectedRoutes>
+          }
+        >
+          <Route path="products" element={<SellerProducts />} />
+          <Route path="add-product" element={<AddProduct />} />
+          <Route path="orders" element={<SellerOrders />} />
+          <Route path="profile" element={<SellerProfile />} />
+          <Route path="logout" element={<LogoutComp />} />
+        </Route>
 
+        {/* --- 404 Catch-All --- */}
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
-
       </Routes>
     </BrowserRouter>
   )
